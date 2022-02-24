8684b1c674e9bbeff5371f3659dd9bf84c06c9b4
jeffjing <zgayjjf@qq.com>
2022-02-24 11:13:37 +0800

    init

---
+++ docs/.vuepress/locales/en.ts
@@ -0,0 +1,12 @@
+import { SidebarConfig } from '@vuepress/theme-default'
+
+export const sidebar: SidebarConfig = [
+  `./guide`,
+  `./home`,
+  `./faq`,
+  `./terminology`,
+]
+
+export const en = {
+  sidebar,
+}
---
+++ docs/guide.md
@@ -0,0 +1,6 @@
+# this is a guide
+# this is a guide
+# this is a guide
+```md:no-v-pre
+1 + 2 + 3 = {{ 1 + 2 + 3 }}
+```
